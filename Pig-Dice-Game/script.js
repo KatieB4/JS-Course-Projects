974891d8603b164f61fef6cc9c5c4acf008222b8
KatieB4 <61069568+KatieB4@users.noreply.github.com>
2023-04-07 15:56:43 -0400

    Add function to set text content for scores

--- Pig-Dice-Game/script.js
+++ Pig-Dice-Game/script.js
@@ -1,37 +1,37 @@
 'use strict';
 
-// let currentScore = Number(document.querySelector('.current-score').textContent);
-let currentScoreText = document.querySelector('.current-score').textContent;
-let currentScoreNum = Number(currentScoreText);
+let currentScore = Number(document.querySelector('.current-score').textContent);
+
+const setTextNum = function (numberPlacement, newNumber) {
+  document.querySelector(`${numberPlacement}`).textContent = newNumber;
+};
 
 document.querySelector('.btn--roll').addEventListener('click', function () {
   const roll = Number(Math.trunc(Math.random() * 6) + 1);
   const dieNum = `dice-${roll}.png`;
   document.querySelector('.dice').setAttribute('src', dieNum);
 
-  // if (roll === 1) {
-  //   currentScore = 0;
-  //   document.querySelector('.current-score').textContent = currentScore;
+  // ---NOTE--- ternary operator prob wont work here because it's more than just setting the currentscore, probably also switching player or declaring winner within
+  // currentScore = roll === 1 ? 0 : currentScore += roll;
 
-  //   // ---TODO--- SWITCH .player--active if class list contains, remove, if doesn't contain, add
-  // } else {
+  if (roll === 1) {
+    currentScore = 0;
 
-  currentScoreNum += roll;
-  // document.querySelector('.current-score').textContent = currentScore;
-  currentScoreText = currentScoreNum;
+    // ---TODO--- SWITCH .player--active if class list contains, remove, if doesn't contain, add
+  } else {
+    currentScore += roll;
 
-  // ---TODO--- set winner
-  // if (currentScore >= 100) {
-  //   console.log('winner!');
-  // }
-  // }
+    // ---TODO--- set winner
+    // if (currentScore >= 100) {
+    //   console.log('winner!');
+    // }
+  }
+  setTextNum('.current-score', currentScore);
 });
 
 document.querySelector('.btn--hold').addEventListener('click', function () {
   let heldScore = Number(document.querySelector('.score').textContent);
   heldScore += currentScore;
-  document.querySelector('.player--active .score').textContent = heldScore;
+  setTextNum('.player--active .score', heldScore);
   currentScore = 0;
 });
-
-// ---TODO--- change querySelectors to variables before changing to numbers??
