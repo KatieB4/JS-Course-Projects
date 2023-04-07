05d0467ea931434ee7c4dd199480f606343b08c0
KatieB4 <61069568+KatieB4@users.noreply.github.com>
2023-04-07 15:41:17 -0400

    Change currentScore setting

--- Pig-Dice-Game/script.js
+++ Pig-Dice-Game/script.js
@@ -1,26 +1,30 @@
 'use strict';
 
-let currentScore = Number(document.querySelector('.current-score').textContent);
+// let currentScore = Number(document.querySelector('.current-score').textContent);
+let currentScoreText = document.querySelector('.current-score').textContent;
+let currentScoreNum = Number(currentScoreText);
 
 document.querySelector('.btn--roll').addEventListener('click', function () {
   const roll = Number(Math.trunc(Math.random() * 6) + 1);
   const dieNum = `dice-${roll}.png`;
   document.querySelector('.dice').setAttribute('src', dieNum);
 
-  if (roll === 1) {
-    currentScore = 0;
-    document.querySelector('.current-score').textContent = currentScore;
+  // if (roll === 1) {
+  //   currentScore = 0;
+  //   document.querySelector('.current-score').textContent = currentScore;
 
-    // ---TODO--- SWITCH .player--active if class list contains, remove, if doens't contain, add
-  } else {
-    currentScore += roll;
-    document.querySelector('.current-score').textContent = currentScore;
+  //   // ---TODO--- SWITCH .player--active if class list contains, remove, if doesn't contain, add
+  // } else {
 
-    // ---TODO--- set winner
-    // if (currentScore >= 100) {
-    //   console.log('winner!');
-    // }
-  }
+  currentScoreNum += roll;
+  // document.querySelector('.current-score').textContent = currentScore;
+  currentScoreText = currentScoreNum;
+
+  // ---TODO--- set winner
+  // if (currentScore >= 100) {
+  //   console.log('winner!');
+  // }
+  // }
 });
 
 document.querySelector('.btn--hold').addEventListener('click', function () {
