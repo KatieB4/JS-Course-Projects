1a18949b383ae762976553b4e6bad1e5cdcbd2ba
KatieB4 <61069568+KatieB4@users.noreply.github.com>
2023-04-08 14:56:51 -0400

    Add reset function to start new game

--- Pig-Dice-Game/script.js
+++ Pig-Dice-Game/script.js
@@ -1,44 +1,87 @@
 'use strict';
 
-let currentScore = Number(document.querySelector('.current-score').textContent);
+let currentScore = Number(
+  document.querySelector('.player--active .current-score').textContent
+);
 
-const setTextNum = function (numberPlacement, newNumber) {
+let heldScore = Number(
+  document.querySelector('.player--active .score').textContent
+);
+
+const players = document.querySelectorAll('.player');
+
+function setTextNum(numberPlacement, newNumber) {
   document.querySelector(`${numberPlacement}`).textContent = newNumber;
-};
-const clickListen = function (btnClicked, handleClick) {
-  document.querySelector(btnClicked).addEventListener('click', handleClick);
-};
+}
 
-clickListen('.btn--roll', rollDie);
+function clickListen(btnClicked, handleClick) {
+  document.querySelector(btnClicked).addEventListener('click', handleClick);
+}
 
 function rollDie() {
   const roll = Number(Math.trunc(Math.random() * 6) + 1);
   const dieNum = `dice-${roll}.png`;
   document.querySelector('.dice').setAttribute('src', dieNum);
 
-  // ---NOTE--- ternary operator prob wont work here because it's more than just setting the currentscore, probably also switching player or declaring winner within
-  // currentScore = roll === 1 ? 0 : currentScore += roll;
+  // ---NOTE--- ternary operator prob wont work here because it's more than just setting the currentscore, probably also switching player or declaring winner within: currentScore = roll === 1 ? 0 : currentScore += roll;
 
   if (roll === 1) {
     currentScore = 0;
-
-    // ---TODO--- SWITCH .player--active if class list contains, remove, if doesn't contain, add
+    setTextNum('.player--active .current-score', currentScore);
+    switchPlayer();
   } else {
     currentScore += roll;
-
-    // ---TODO--- set winner, not currentScore, but player's score
+    setTextNum('.player--active .current-score', currentScore);
+    // ---TODO--- set winner, not with currentScore, but with player's score
     // if (currentScore >= 100) {
     //   console.log('winner!');
     // }
+    // console.log(
+    //   typeof document.querySelector('.player--active .score').textContent
+    // );
+    // console.log(document.querySelector('.player--active .score').textContent);
+
+    // if (document.querySelector('.player--active .score').textContent >= '10') {
+    //   console.log('winner');
+    // }
   }
-  setTextNum('.current-score', currentScore);
 }
 
-clickListen('.btn--hold', saveScore);
-
 function saveScore() {
-  let heldScore = Number(document.querySelector('.score').textContent);
   heldScore += currentScore;
   setTextNum('.player--active .score', heldScore);
   currentScore = 0;
+  setTextNum('.player--active .current-score', currentScore);
+  switchPlayer();
 }
+
+function switchPlayer() {
+  for (let i = 0; i < players.length; i++) {
+    // if (players[i].classList.contains('player--active')) {
+    //   players[i].classList.remove('player--active');
+    // } else {
+    //   players[i].classList.add('player--active');
+    // }
+    players[i].classList.toggle('player--active');
+  }
+}
+
+function startOver() {
+  // location.reload();
+  heldScore = 0;
+  currentScore = 0;
+
+  for (let i = 0; i < players.length; i++) {
+    setTextNum(`.player--${[i]} .score`, heldScore);
+    setTextNum(`.player--${[i]} .current-score`, heldScore);
+  }
+
+  document.querySelector('.player--0').classList.add('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
+}
+
+clickListen('.btn--roll', rollDie);
+
+clickListen('.btn--hold', saveScore);
+
+clickListen('.btn--new', startOver);
