fb9c4ab7f82e9ffacf36a6e581793496bf8cb659
KatieB4 <61069568+KatieB4@users.noreply.github.com>
2023-04-11 13:01:53 -0400

    Add switch stored score function to fix previous bugs

--- Pig-Dice-Game/script.js
+++ Pig-Dice-Game/script.js
@@ -4,9 +4,15 @@ let currentScore = Number(
   document.querySelector('.player--active .current-score').textContent
 );
 
-let heldScore = Number(
-  document.querySelector('.player--active .score').textContent
-);
+let storedScore = 0;
+
+// let heldScore1 = Number(
+//   document.querySelector('.player--0 .score').textContent
+// );
+
+// let heldScore2 = Number(
+//   document.querySelector('.player--1  .score').textContent
+// );
 
 const players = document.querySelectorAll('.player');
 
@@ -32,59 +38,96 @@ function rollDie() {
   } else {
     currentScore += roll;
     setTextNum('.player--active .current-score', currentScore);
-    // ---TODO--- set winner, not with currentScore, but with player's score
-    // console.log(document.querySelector('.player--active .score').textContent);
-    // if (active player heldScore >= 10) {
-    //   console.log('winner!');
-    // }
-    // console.log(
-    //   typeof document.querySelector('.player--active .score').textContent
-    // );
-    // console.log(document.querySelector('.player--active .score').textContent);
-
-    // if (document.querySelector('.player--active .score').textContent >= '10') {
-    //   console.log('winner');
-    // }
   }
 }
 
-// ---TODO--- make new function to update scores
+function switchStoredScore() {
+  storedScore = Number(
+    document.querySelector('.player--active .score').textContent
+  );
+  return storedScore;
+}
 
 function saveScore() {
   if (currentScore != 0) {
-    heldScore += currentScore;
-    setTextNum('.player--active .score', heldScore);
-    // ---BUG--- this is currently replacing the score each time instead of adding to it
+    storedScore += currentScore;
+    setTextNum('.player--active .score', storedScore);
+
     currentScore = 0;
-    heldScore = 0;
     setTextNum('.player--active .current-score', currentScore);
+
     switchPlayer();
   }
 }
 
+// function updateSavedScore2() {
+//   if (currentScore != 0) {
+//     if (
+//       document.querySelector('.player--active').classList.contains('player--0')
+//     ) {
+//       heldScore1 += currentScore;
+//       setTextNum('.player--active .score', heldScore1);
+//       if (document.querySelector('.player--active .score').textContent >= 10) {
+//         console.log('winner!');
+//       }
+//     }
+
+//     if (
+//       document.querySelector('.player--active').classList.contains('player--1')
+//     ) {
+//       heldScore2 += currentScore;
+//       setTextNum('.player--active .score', heldScore2);
+//       if (document.querySelector('.player--active .score').textContent >= 10) {
+//         console.log('winner!');
+//       }
+//     }
+//     currentScore = 0;
+//     setTextNum('.player--active .current-score', currentScore);
+
+//     switchPlayer();
+//   }
+// }
+
+// function saveScore() {
+//   if (currentScore != 0) {
+//     console.log(heldScore);
+//     heldScore += currentScore;
+//     console.log(heldScore);
+//     setTextNum('.player--active .score', heldScore);
+//     currentScore = 0;
+//     // ---BUG--- this is currently replacing the score each time instead of adding to it
+//     heldScore = 0;
+//     console.log(heldScore);
+//     // ---BUG--- without heldScore reset to zero, both sides accumulate in one- make into loop for each half?
+//     setTextNum('.player--active .current-score', currentScore);
+//     console.log(heldScore);
+//     switchPlayer();
+//     console.log(heldScore);
+//   }
+// }
+
 function switchPlayer() {
   for (let i = 0; i < players.length; i++) {
-    // if (players[i].classList.contains('player--active')) {
-    //   players[i].classList.remove('player--active');
-    // } else {
-    //   players[i].classList.add('player--active');
-    // }
     players[i].classList.toggle('player--active');
   }
+  switchStoredScore();
 }
 
 function startOver() {
   // location.reload();
-  heldScore = 0;
+
+  storedScore = 0;
   currentScore = 0;
 
   for (let i = 0; i < players.length; i++) {
-    setTextNum(`.player--${[i]} .score`, heldScore);
-    setTextNum(`.player--${[i]} .current-score`, heldScore);
+    setTextNum(`.player--${i} .score`, storedScore);
+    setTextNum(`.player--${i} .current-score`, currentScore);
+
+    document.querySelector(`.player--${i}`).classList.remove('player--active');
   }
 
   document.querySelector('.player--0').classList.add('player--active');
-  document.querySelector('.player--1').classList.remove('player--active');
+  // document.querySelector('.player--1').classList.remove('player--active');
 }
 
 clickListen('.btn--roll', rollDie);
